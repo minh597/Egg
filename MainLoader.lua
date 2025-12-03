da86edfe4d9eea6223b3a9f6d628e10a73328bea
minh597 <minhdangtran1012@gmail.com>
2025-12-03 18:21:15 +0700

    Create MainLoader.lua

---
+++ MainLoader.lua
@@ -0,0 +1,12 @@
+local gameID = game.GameId
+
+local scripts = {
+    [2232507648] = "https://raw.githubusercontent.com/minh597/Egg/refs/heads/main/Loader/untitledAttackOnTitan.lua"
+}
+
+if scripts[gameID] then
+    loadstring(game:HttpGet(scripts[gameID]))()
+else
+    local player = game.Players.LocalPlayer
+    player:Kick("Haven't support this game yet !")
+end
