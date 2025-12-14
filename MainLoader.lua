28faec3e0e0ba4b0471b19d977fc482324282eb5
minh597 <minhdangtran1012@gmail.com>
2025-12-14 16:40:01 +0700

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -4,7 +4,8 @@ local scripts = {
     [2232507648] = "https://raw.githubusercontent.com/minh597/Egg/refs/heads/main/Loader/untitledAttackOnTitan.lua",
     [210851291] = "https://raw.githubusercontent.com/minh597/Egg/refs/heads/main/Loader/BuildABoatForTreasure.lua",
     [111958650] = "https://raw.githubusercontent.com/minh597/Egg/refs/heads/main/Loader/ArsenalAimbot.lua",
-    [8599518829] = "https://raw.githubusercontent.com/minh597/Egg/refs/heads/main/Loader/BuildaStoneMiner.lua"
+    [8599518829] = "https://raw.githubusercontent.com/minh597/Egg/refs/heads/main/Loader/BuildaStoneMiner.lua",
+    [9105897479] = "https://raw.githubusercontent.com/minh597/Egg/refs/heads/main/Loader/YourBank!.lua"
 }
 
 if scripts[gameID] then
